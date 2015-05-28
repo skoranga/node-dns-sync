77d0159f1f4182f10418805c6abd1de54067eeda
Richard Lyon <richthegeek@gmail.com>
2015-05-28 12:46:46 +0100

    Allow resolving things other than A records, fix issue with buffers

--- scripts/dns-lookup-script.js
+++ scripts/dns-lookup-script.js
@@ -1,15 +1,21 @@
 'use strict';
 
 var dns = require('dns'),
-    name = process.argv[2],
     debug = require('debug')('dns-sync');
 
-dns.lookup(name, function (err, ip) {
+for (var i = 0; i < process.argv.length; i++) {
+	if (process.argv[i].indexOf('dns-lookup-script') >= 0) {
+		var name = process.argv[i + 1];
+		var type = process.argv[i + 2] || 'A';
+	}
+}
+
+dns.resolve(name, type, function (err, ip) {
     if (err) {
         process.exit(1);
         debug(err);
     } else {
         debug(name, 'resolved to', ip);
-        process.stdout.write(ip);
+        process.stdout.write(JSON.stringify(ip));
     }
 });
